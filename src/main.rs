db69d042ac01f071590ad0e82d1d63bb25eb4062
Remi Rampin <remi@rampin.org>
2021-10-16 12:33:37 -0400

    Reopen file on RENAME, REMOVE

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,81 @@ struct Data {
     request_count: IntCounterVec,
 }
 
+fn watch_log(filename: &Path, log_parser: &LogParser, data: Arc<Mutex<Data>>) -> Result<(), Box<dyn std::error::Error>> {
+    let mut file = std::fs::OpenOptions::new().read(true).open(&filename)?;
+
+    let (tx, rx) = std::sync::mpsc::channel();
+    let mut watcher: RecommendedWatcher = RecommendedWatcher::new_raw(tx)?;
+    watcher.watch(&filename, notify::RecursiveMode::NonRecursive)?;
+    let mut offset = file.seek(SeekFrom::End(0))?;
+
+    let mut buffer = String::new();
+
+    // Wait for events
+    loop {
+        let event: notify::RawEvent = rx.recv()?;
+
+        eprintln!("event: {:?}", event);
+
+        let reopen = match event.op {
+            Ok(op) if op.contains(notify::op::Op::RENAME | notify::op::Op::REMOVE) => {
+                eprintln!("File moved, restarting watch");
+                true
+            }
+            Err(e) => return Err(e.into()),
+            _ => false,
+        };
+
+        if reopen {
+            return Ok(());
+        }
+
+        // Check size
+        let size = file.seek(SeekFrom::End(0))?;
+        if size < offset {
+            eprintln!("Truncation detected ({} -> {})", offset, size);
+            offset = size;
+        }
+
+        // Read
+        file.seek(SeekFrom::Start(offset))?;
+        let res = file.read_to_string(&mut buffer)? as u64;
+        offset += res;
+
+        // Split into lines
+        let mut read_to = 0;
+        while let Some(ln) = buffer[read_to..].find('\n') {
+            let line = &buffer[read_to..read_to + ln];
+            eprintln!("line: {:?}", line);
+            read_to += ln + 1;
+
+            let values = log_parser.parse(line)?;
+            let mut remote_user = None;
+            let mut status = None;
+            let mut vhost: Option<String> = None;
+            for value in values {
+                match value {
+                    LogValue::RemoteUser(s) => remote_user = Some(s),
+                    LogValue::Request(_) => {}
+                    LogValue::Status(i) => status = Some(i),
+                    LogValue::Host(s) => vhost = Some(s),
+                    LogValue::BodyBytesSent(_) => {}
+                    LogValue::Other(_, _) => {}
+                }
+            }
+
+            let data = data.lock().unwrap();
+            data.request_count.with_label_values(&[
+                &status.map(|i| Owned(format!("{}", i))).unwrap_or(Borrowed("unk")),
+                vhost.as_ref().map(|s| -> &str { s }).unwrap_or("unk"),
+            ]).inc();
+        }
+
+        // Discard the lines from the buffer
+        buffer.drain(0..read_to);
+    }
+}
+
 impl LogCollector {
     fn new(log_parser: LogParser, filename: PathBuf) -> Result<LogCollector, notify::Error> {
         let data = Data {
@@ -39,62 +114,14 @@ impl LogCollector {
 
         let data_rc = data.clone();
         std::thread::spawn(move || {
-            let mut file = std::fs::OpenOptions::new().read(true).open(&filename).expect("IO");
-
-            let (tx, rx) = std::sync::mpsc::channel();
-            let mut watcher: RecommendedWatcher = RecommendedWatcher::new_raw(tx).expect("notify");
-            watcher.watch(&filename, notify::RecursiveMode::NonRecursive).expect("notify");
-            let mut offset = file.seek(SeekFrom::End(0)).unwrap();
-
-            let mut buffer = String::new();
-
-            // Wait for events
             loop {
-                rx.recv().expect("notify");
-
-                // Check size
-                let size = file.seek(SeekFrom::End(0)).expect("IO");
-                if size < offset {
-                    eprintln!("Truncation detected ({} -> {})", offset, size);
-                    offset = size;
-                }
-
-                // Read
-                file.seek(SeekFrom::Start(offset)).expect("IO");
-                let res = file.read_to_string(&mut buffer).expect("IO") as u64;
-                offset += res;
-
-                // Split into lines
-                let mut read_to = 0;
-                while let Some(ln) = buffer[read_to..].find('\n') {
-                    let line = &buffer[read_to..read_to + ln];
-                    eprintln!("line: {:?}", line);
-                    read_to += ln + 1;
-
-                    let values = log_parser.parse(line).unwrap();
-                    let mut remote_user = None;
-                    let mut status = None;
-                    let mut vhost: Option<String> = None;
-                    for value in values {
-                        match value {
-                            LogValue::RemoteUser(s) => remote_user = Some(s),
-                            LogValue::Request(_) => {}
-                            LogValue::Status(i) => status = Some(i),
-                            LogValue::Host(s) => vhost = Some(s),
-                            LogValue::BodyBytesSent(_) => {}
-                            LogValue::Other(_, _) => {}
-                        }
+                match watch_log(&filename, &log_parser, data_rc.clone()) {
+                    Ok(()) => {}
+                    Err(e) => {
+                        eprintln!("{}", e);
+                        std::process::exit(1);
                     }
-
-                    let data = data_rc.lock().unwrap();
-                    data.request_count.with_label_values(&[
-                        &status.map(|i| Owned(format!("{}", i))).unwrap_or(Borrowed("unk")),
-                        vhost.as_ref().map(|s| -> &str { s }).unwrap_or("unk"),
-                    ]).inc();
                 }
-
-                // Discard the lines from the buffer
-                buffer.drain(0..read_to);
             }
         });
 
